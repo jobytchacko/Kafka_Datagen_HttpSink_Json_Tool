cc3a9afcc13c3d74412fdced6298e9a05d6b0c56
Lithin Thomas <72386223+Lithin87@users.noreply.github.com>
2023-06-01 16:39:54 +0000

    .

--- index.cjs
+++ index.cjs
@@ -3,7 +3,8 @@
  */
 const projectId = 'ecstatic-cosmos-387220';
 
-const { Compute }  = require('@google-cloud/compute');
+// import  Compute from '@google-cloud/compute';
+const { InstancesClient } = require('@google-cloud/compute');
 
 // List all instances in the specified project.
 async function listAllInstances() {
@@ -32,4 +33,34 @@ async function listAllInstances() {
   }
 }
 
-listAllInstances();
+// listAllInstances();
+
+
+
+const computevm = new InstancesClient();
+const zone = computevm.zone('your-zone'); // Replace 'your-zone' with your desired zone, e.g., 'us-central1-a'
+
+const vmConfig = {
+  name: 'node-vm', // Replace 'your-vm-name' with your desired VM name
+  machineType: 'n1-standard-1', // Replace 'your-machine-type' with the desired machine type, e.g., 'n1-standard-1'
+  disks: [
+    {
+      boot: true,
+      initializeParams: {
+        sourceImage: 'projects/debian-cloud/global/images/debian-10-buster-v20220929', // Replace 'your-image' with the desired image, e.g., 'projects/debian-cloud/global/images/debian-10-buster-v20220929'
+      },
+    },
+  ],
+};
+
+async function createVM() {
+  try {
+    const [vm] = await zone.createVM(vmConfig);
+    console.log(`VM created successfully: ${vm.name}`);
+  } catch (err) {
+    console.error('Error creating VM:', err);
+  }
+}
+
+createVM();
+
