f604791a8083e89e29705ab1bb602bf1d265301b
Lithin87 <72386223+Lithin87@users.noreply.github.com>
2023-09-03 23:34:25 +0530

    .

--- src/RESTAPI-Menu.js
+++ src/RESTAPI-Menu.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 const app = express();
 app.use(express.json());
 dotenv.config();
-const frontend = process.env.FRONTEND_HOST;
+const frontend = "https://kafka-datagen-httpsink-json-tool-xdms6u477a-uc.a.run.app";
 console.log(frontend);
 app.use(cors({ origin: frontend })); 
 
